5b4d1cb9e68bf4c6e90caa5f0e80388790b8f58a
MeierLC <lukascornelmeier@gmx.de>
2024-01-12 09:01:56 +0100

    SQL-Script zum Testen der Funktionen

---
+++ Funktionen/Funktionstests.sql
@@ -0,0 +1,14 @@
+SELECT getAnwesenheitsliste(1);
+
+/*Anna Müller*/
+SELECT getDozentTermine(1,"WS2023/2024");
+
+SELECT getDualestudenten();
+
+/*Königsmann*/
+SELECT getFehltage(456789);
+
+/*Königsmann*/
+SELECT getNextTermin(456789,"Web-Engineering");
+
+SELECT isUni(456789,"2024-10-03");
